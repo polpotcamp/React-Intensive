d0760d7f0247ba314dbf17c56db23af2c2b4b5ab
Роман Маскаев <subulka@mail.ru>
2025-06-30 15:11:00 +0300

    add postList

---
+++ src/app/App.tsx
@@ -0,0 +1,14 @@
+
+import PostList from '../widgets/PostList/PostList'
+import posts from '../Posts.json'
+function App() {
+
+
+  return (
+    <>
+    <PostList posts={posts}/>
+    </>
+  )
+}
+
+export default App
---
+++ src/entities/post/ui/PostCard.tsx
@@ -0,0 +1,17 @@
+import type { FC } from "react";
+import type { TPost } from "../../../types/PostType";
+import styles from './PostCard.module.css'
+interface PostCardProps {
+    post: TPost
+}
+ 
+const PostCard: FC<PostCardProps> = ({post}) => {
+    return (
+        <div className={`${styles.postContainer}`}>
+            <p  className={`${styles.postTitle}`}>{post.title}</p>
+            <p  className={`${styles.postText}`}>{post.text}</p>
+        </div>
+      );
+}
+ 
+export default PostCard;
