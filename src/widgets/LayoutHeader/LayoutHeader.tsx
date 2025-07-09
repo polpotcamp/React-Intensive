edbd57a840bde6e8714c6b07a303dfe85e273912
Роман Маскаев <subulka@mail.ru>
2025-07-09 18:34:20 +0300

    fix LayoutHeader

--- src/widgets/LayoutHeader/LayoutHeader.tsx
+++ src/widgets/LayoutHeader/LayoutHeader.tsx
@@ -4,14 +4,16 @@ import Modal from "../../shared/ui/Modal/Modal";
 import { Button } from "../../shared/ui/Button/Button";
 const LayoutHeader: FC = () => {
   const [isModalOpen, setModalOpen] = React.useState(false);
+  const toggleModal = () => setModalOpen(!isModalOpen);
+  const closeModal = () => setModalOpen(false);
   return (
     <header>
-      <Button onClick={() => setModalOpen(!isModalOpen)}>
+      <Button onClick={toggleModal}>
         Открыть модальное окно с информацией о проекте
       </Button>
       <ThemeSwitcher />
       {isModalOpen && (
-        <Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)}>
+        <Modal isOpen={isModalOpen} onClose={closeModal}>
           Информация о проекте Информация о проекте Информация о проекте
           Информация о проекте
         </Modal>
